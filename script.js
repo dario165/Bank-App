abab4e313c4203dd405f5c0a4d394cd36a41221d
Dario <darioalexandre165@gmail.com>
2022-11-07 15:30:16 +0000

    add pay credit card

--- script.js
+++ script.js
@@ -13,27 +13,23 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Joao Almeida',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.2,
-  pin: 2222,
+  owner: 'Gold Card',
+  movements: [],
+  
+ 
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 1.2,
-  pin: 3333,
+  owner: 'Blue Card',
+  movements: [],
+  
+
 };
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1.2,
-  pin: 4444,
-};
 
-const accounts = [account1, account2, account3, account4];
+
+
+const accounts = [account1, account2, account3, ];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -163,6 +159,35 @@ btnLogin.addEventListener('click', function (e) {
   }
 });
 
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+
+
+
+
+
+  
 
 
 
